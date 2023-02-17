e369f027b8371af04dd41628809ea4f83e14406d
laurenduffitt <laurenduffitt@gmail.com>
2023-02-17 17:16:14 -0500

    things

--- Train Model/TrainModelTestUI_v1.py
+++ Train Model/TrainModelTestUI_v1.py
@@ -23,7 +23,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         #light status change
         self.intLights.stateChanged.connect(self.clickBox)
-        #self.extLights.stateChanged.connect(self.clickBox)
+        self.extLights.stateChanged.connect(self.clickBox)
+        self.headlights.stateChanged.connect(self.clickBox)
+        self.leftDoor.stateChanged.connect(self.clickBox)
+        self.rightDoor.stateChanged.connect(self.clickBox)
 
     #temp set function
     def tempInputfunc(self):
@@ -46,19 +49,45 @@ class MainWindow(QtWidgets.QMainWindow):
             self.checkBox.setIcon(icon)
             self.checkBox.setIconSize(QSize(300, 300))
 
+        #internal lights
         if self.intLights.isChecked():
                 self.intLightLabel.setStyleSheet("background-color: green")
+                self.intLightLabel.setText("ON")
         else:
             self.intLightLabel.setStyleSheet("background-color: red")
+            self.intLightLabel.setText("OFF")
 
-       # if self.extLights.isChecked():
-       #         self.extLightLabel.setStyleSheet("background-color: green")
-       # else:
-       #     self.extLightLabel.setStyleSheet("background-color: red")
+        #external lights
+        if self.extLights.isChecked():
+                self.extLightLabel.setStyleSheet("background-color: green")
+                self.extLightLabel.setText("ON")
+        else:
+            self.extLightLabel.setStyleSheet("background-color: red")
+            self.extLightLabel.setText("OFF")
 
+        #headlights
+        if self.headlights.isChecked():
+                self.headlightLabel.setStyleSheet("background-color: green")
+                self.headlightLabel.setText("ON")
+        else:
+            self.headlightLabel.setStyleSheet("background-color: red")
+            self.headlightLabel.setText("OFF")
 
+        #Left doors
+        if self.leftDoor.isChecked():
+                self.lDoorLabel.setStyleSheet("background-color: green")
+                self.lDoorLabel.setText("ON")
+        else:
+            self.lDoorLabel.setStyleSheet("background-color: red")
+            self.lDoorLabel.setText("OFF")
 
-            
+        #Right doors
+        if self.rightDoor.isChecked():
+                self.rDoorLabel.setStyleSheet("background-color: green")
+                self.rDoorLabel.setText("ON")
+        else:
+            self.rDoorLabel.setStyleSheet("background-color: red")
+            self.rDoorLabel.setText("OFF")
     
 
 #end class definition
