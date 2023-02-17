ecc4a0478a0fc43a673f2e44a3615f2ce5a55811
Jacob Kerstetter <jacobrkerstetter@gmail.com>
2023-02-17 16:24:56 -0500

    added line and section classes

---
+++ Section.py
@@ -0,0 +1,19 @@
+import Block
+
+class Section:
+    # declare attributes
+    blocks = []
+
+    # constructor
+    def __init__(self, sectionName):
+        self.section = sectionName
+
+    # declare methods
+    def addBlock(block):
+        blocks.append(block)
+
+    def removeBlock(block):
+        blocks.remove(block)
+
+    def clearBlocks():
+        blocks.clear()
