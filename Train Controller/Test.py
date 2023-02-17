0400ee8d5805276feae6a0b77d7c39a9c4bebb79
headhunter22 <jdh161@pitt.edu>
2023-02-17 17:46:04 -0500

    QtCreator working

--- Train Controller/Test.py
+++ Train Controller/Test.py
@@ -1,20 +1,32 @@
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
+from PyQt6 import QtGui
 import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.buttonClick)
+        self.setWindowTitle("Train Controller")
+        self.resize(980, 620)
 
-        self.setCentralWidget(button)
+        # Emergency Brake button init #
+        button = QPushButton('EMERGENCY BRAKE', self)
+        #txt = QLabel("Emergency Brake Disengaged", self)
+        button.setGeometry(50, 50, 200, 200)
+        #txt.setGeometry(75, 210, 200, 100)
+        button.setCheckable(False)
+        button.clicked.connect(self.EBClick)
+        #button.setStyleSheet("QPushButton { background-color : rgb(255,255,255); color: rgb(0,0,0)}"
 
-    def buttonClick(self):
-        print("Clicked")
+
+
+    def EBClick(self):
+            self.setStyleSheet("QPushButton { background-color : rgb(255,0,0)}")
+            print("Engaged")
+            txt = QLabel("Emergency Brake Engaged", self)
+            txt.setGeometry(75, 210, 200, 100)
+
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
