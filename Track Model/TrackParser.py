65f39a96291b148593d890e502e631b7994580ac
Jacob Kerstetter <jacobrkerstetter@gmail.com>
2023-02-18 10:47:21 -0500

    finished block, line, section classes and working on track model UI

--- Track Model/TrackParser.py
+++ Track Model/TrackParser.py
@@ -1,35 +1,8 @@
 import csv
-import sqlite3
+import Line, Section, Block
 
-# db to store table
-connection = sqlite3.connect('tracklayout.db')
-cursor = connection.cursor()
+with open('Track Layout.csv', newline='') as csvfile:
+    trackReader = csv.reader(csvfile)
 
-# define the table
-tracklayout = '''CREATE TABLE tracklayout (
-             line TEXT,
-             section TEXT,
-             block_number TEXT,
-             block_length INTEGER,
-             block_grade,
-             speed_limit,
-             infrastructure TEXT,
-             blank,
-             elevation INTEGER,
-             cum_elevation INTEGER); 
-           '''
-
-# create table in db
-cursor.execute(tracklayout)
-
-# open file and insert into table
-file = open('Track Layout.csv')
-content = csv.reader(file)
-
-# query to insert data
-insert = "INSERT INTO tracklayout (line, section, block_number, block_length, block_grade, speed_limit, infrastructure, blank, elevation, cum_elevation) VALUES(?,?,?,?,?,?,?,?,?,?)"
-cursor.executemany(insert, content)
-
-# commit changes and close
-connection.commit()
-connection.close()
+    for row in trackReader:
+        # if line does not exist 
