fd3ca8fd11f5b8933c0ab904e27075301ad52918
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-02-19 15:39:04 -0500

    parsing to UIgit add -Agit add -A

--- Track Model/TrackParser.py
+++ Track Model/TrackParser.py
@@ -1,45 +1,41 @@
-import csv, sys
+import csv
 
-sys.path.append('../ECE1140-TEAM-1/')
-
+# import classes
 from Track import Track
 from Line import Line
 from Section import Section
 from Block import Block
 
-track = Track()
+def parseTrack(filename):
+    # instantiate the track
+    track = Track()
 
-with open('Track Layout.csv', newline='') as csvfile:
-    trackReader = csv.reader(csvfile)
+    with open(filename, newline='') as csvfile:
+        trackReader = csv.reader(csvfile)
 
-    # skip first line of headers
-    next(trackReader)
+        # skip first line of headers
+        next(trackReader)
 
-    # for each real row in csv, read in data
-    for row in trackReader:
-        # define objects for row line, section, and block
-        newLine = Line(row[0])
-        newSection = Section(row[1])
-        newBlock = Block(row[2:])
+        # for each real row in csv, read in data
+        for row in trackReader:
+            # define objects for row line, section, and block
+            newLine = Line(row[0])
+            newSection = Section(row[1])
+            newBlock = Block(row[2:])
 
-        # if line does not exist, add it to track lines
-        if not track.hasLine(newLine.lineName):
-            track.addLine(newLine)
-        currLine = track.getLine(newLine.lineName)
+            # if line does not exist, add it to track lines
+            if not track.hasLine(newLine.lineName):
+                track.addLine(newLine)
+            currLine = track.getLine(newLine.lineName)
 
-        # if section does not exist on the line, add it to the line
-        if not currLine.hasSection(newSection.sectionName):
-            currLine.addSection(newSection)
-        currSection = currLine.getSection(newSection.sectionName)
+            # if section does not exist on the line, add it to the line
+            if not currLine.hasSection(newSection.sectionName):
+                currLine.addSection(newSection)
+            currSection = currLine.getSection(newSection.sectionName)
 
-        # if block does not exist in the section, add it to the block
-        if not currSection.hasBlock(newBlock.blockName):
-            currSection.addBlock(newBlock)
+            # if block does not exist in the section, add it to the block
+            if not currSection.hasBlock(newBlock.blockName):
+                currSection.addBlock(newBlock)
+                currLine.addBlock(newBlock)
 
-
-for line in track.lines:
-    print(line.lineName)
-    for section in line.sections:
-        print('\t' + section.sectionName)
-        for block in section.blocks:
-            print('\t\t' + block.blockName)
+    return track
