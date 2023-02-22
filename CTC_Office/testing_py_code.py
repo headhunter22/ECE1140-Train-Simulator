827cbfd12d08e4e9a0db69161c54fd16a1d88148
AdamKort <ajk179@pitt.edu>
2023-02-22 01:02:12 -0500

    ctc main ui designed - no functionality yet

--- CTC_Office/testing_py_code.py
+++ CTC_Office/testing_py_code.py
@@ -1,7 +1,48 @@
-import pandas as pd
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QStackedWidget, QLabel
 
-track = pd.read_csv('Track Layout.csv', usecols=['Line', 'Block Number', 'Block Length (m)', 'Speed Limit (Km/Hr)', 'Infrastructure'])
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-print(track)
+        # Create a stacked widget and add widgets to it
+        self.stacked_widget = QStackedWidget()
+        self.page1 = QWidget()
+        self.page2 = QWidget()
+        self.stacked_widget.addWidget(self.page1)
+        self.stacked_widget.addWidget(self.page2)
 
-blockLength = track['Block Length (m)']
+        # Add some content to the pages
+        label1 = QLabel("This is page 1")
+        label2 = QLabel("This is page 2")
+        layout1 = QVBoxLayout(self.page1)
+        layout2 = QVBoxLayout(self.page2)
+        layout1.addWidget(label1)
+        layout2.addWidget(label2)
+
+        # Create buttons to switch between pages
+        self.button1 = QPushButton("Page 1")
+        self.button2 = QPushButton("Page 2")
+
+        # Connect the buttons to the switch_page method
+        self.button1.clicked.connect(lambda: self.switch_page(0))
+        self.button2.clicked.connect(lambda: self.switch_page(1))
+
+        # Add widgets to the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.stacked_widget)
+        self.setLayout(layout)
+
+        # Set initial view
+        self.switch_page(0)
+
+    def switch_page(self, index):
+        self.stacked_widget.setCurrentIndex(index)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
