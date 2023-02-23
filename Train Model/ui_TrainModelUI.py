3da44153d75d912c1d93ab6062b0bd77cd1fbc7e
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-02-22 20:41:19 -0500

    final track model changes

--- Train Model/ui_TrainModelUI.py
+++ Train Model/ui_TrainModelUI.py
@@ -157,7 +157,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 817, 17))
+        self.menubar.setGeometry(QRect(0, 0, 817, 26))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
