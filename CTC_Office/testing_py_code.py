b17d3e6b73c131cc7323ff7ff6552b16face53a5
AdamKort <ajk179@pitt.edu>
2023-02-23 10:26:12 -0500

    ctc began working on dispatch functionality

--- CTC_Office/testing_py_code.py
+++ CTC_Office/testing_py_code.py
@@ -1,18 +1,38 @@
-from PyQt6.QtGui import QColor, QBrush
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt6.QtCore import QTime
+from PyQt6.QtWidgets import QApplication, QDialog, QGridLayout, QLabel, QTimeEdit, QPushButton
 
-app = QApplication([])
-window = QMainWindow()
-table_widget = QTableWidget()
+class MyDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-table_widget.setRowCount(3)
-table_widget.setColumnCount(3)
+    def initUI(self):
+        # Create a QTimeEdit widget
+        self.time_edit = QTimeEdit()
+        self.time_edit.setDisplayFormat("hh:mm:ss")
 
-# Add a new item to row 0, column 0 with a green background
-item = QTableWidgetItem('Hello')
-item.setBackground(QBrush(QColor('green')))
-table_widget.setItem(0, 0, item)
+        # Create a button to read the time from the QTimeEdit widget
+        self.read_time_button = QPushButton("Read Time")
+        self.read_time_button.clicked.connect(self.handleReadTimeClicked)
 
-window.setCentralWidget(table_widget)
-window.show()
-app.exec()
+        # Create a label to display the time
+        self.time_label = QLabel()
+
+        # Create a grid layout and add the widgets to it
+        layout = QGridLayout()
+        layout.addWidget(QLabel("Time:"), 0, 0)
+        layout.addWidget(self.time_edit, 0, 1)
+        layout.addWidget(self.read_time_button, 1, 0)
+        layout.addWidget(self.time_label, 1, 1)
+        self.setLayout(layout)
+
+    def handleReadTimeClicked(self):
+        # Read the time from the QTimeEdit widget and display it in the label
+        time = self.time_edit.time()
+        self.time_label.setText(time.toString("hh:mm:ss"))
+
+if __name__ == '__main__':
+    app = QApplication([])
+    dialog = MyDialog()
+    dialog.show()
+    app.exec()
