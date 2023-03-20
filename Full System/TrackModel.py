8b832dc961316b0b78cdad44642ec48e3f89395d
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-03-20 17:16:24 -0400

    track model class ready to receive

--- Full System/TrackModel.py
+++ Full System/TrackModel.py
@@ -1,13 +1,14 @@
-from TrackModelSend import TrackModelSend
+import sys, os
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import QSize, QObject, QThread, pyqtSignal
+from PyQt6 import uic
 
-class TrackModel:
-    def __init__(self, track):
-        #self.Rx = TrackModelReceive()
-        self.Tx = TrackModelSend() 
-        self.track = track
+class TrackModel(QObject):
+    def __init__(self, waysideController):
+        super().__init__()
+        self.wayside = waysideController
+        self.suggSpeed = 0
 
-    def receive(self):
-        self.Rx.pullInfo()
-
-    def send(self):
-        self.Tx.sendInfo()
+    def suggSpeedReceived(self, suggSpeed):
+        self.suggSpeed = suggSpeed
+        print("suggested speed from Wayside to Track Model " + str(self.suggSpeed)) 
