3f3c4c935ddb89cd942e285d604bf428f00a8c91
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-03-27 16:43:05 -0400

    THE CLOCK WORKS

--- Full System/Clock.py
+++ Full System/Clock.py
@@ -1,30 +1,16 @@
-from PyQt6 import QtCore, QtWidgets, uic
+from PyQt6 import QtCore
 import sys
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        uic.loadUi("ClockTest.ui", self)
-        self.setWindowTitle('Track Model UI')
-
-        self.pushButton.clicked.connect(self.start)
-        self.pushButton_2.clicked.connect(self.stop)
-
+class Clock():
+    def __init__(self):
         self.clock = QtCore.QTimer()
         self.time = 0
-        self.started = False
 
-    def start(self):
-        print('Start Timer')
+    def tenTimesSpeed(self):
+        self.clock.setInterval(100)
 
-        self.clock.start(1000)
-        self.clock.setInterval(1000)
-
-        self.clock.timeout.connect(self.changeLabel)
-
-    def stop(self):
-        print('Stop Timer')
-        self.clock.stop()
+    def fiftyTimesSpeed(self):
+        self.clock.setInterval(20)
     
     def changeLabel(self):
         self.time += 1
@@ -34,8 +20,3 @@ class MainWindow(QtWidgets.QMainWindow):
         secs = (mins - int(mins)) * 60
 
         self.label.setText(f'{int(hrs):02d}' + ':' + f'{int(mins):02d}' + ':' + f'{int(secs):02d}')
-
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
