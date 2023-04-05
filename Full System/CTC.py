28a634cf16d6818e255d8a2c33658cfb6a132c0b
Adam Kort <ajk179@pitt.edu>
2023-04-05 16:17:03 -0400

    trying to get rid of pycache

--- Full System/CTC.py
+++ Full System/CTC.py
@@ -8,7 +8,7 @@ from Line import Line
 from signals import signals
 sys.dont_write_bytecode = True
 
-#please work please
+#please work please pretty pleasse
 
 class CTC(QObject):
     def __init__(self, track):
