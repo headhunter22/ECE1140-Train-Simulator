3fc8df472c97918b0372f76fdd8edab52620716a
Jacob Kerstetter <83893427+jacobrkerstetter@users.noreply.github.com>
2023-04-18 17:34:47 -0400

    fixed csv

--- Full System/Beacon.py
+++ Full System/Beacon.py
@@ -7,9 +7,8 @@ class Beacon:
     def __init__(self, station=None, stationSide=None, swFrom=0, swTo=0):
         # for station beacons
         self.stationName = station
-        self.stataionSide = stationSide
+        self.stationSide = stationSide
 
         # for switch beacons
         self.switchFrom = swFrom
         self.switchTo = swTo
-        
